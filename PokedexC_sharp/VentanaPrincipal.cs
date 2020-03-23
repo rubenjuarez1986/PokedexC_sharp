d634973db59b5ed64ab8a34074a7bb840d51ff68
fraxito <fraxito@gmail.com>
2020-03-24 00:10:33 +0100

    Agregar archivos de proyecto.

---
+++ PokedexC_sharp/VentanaPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokedexC_sharp
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
